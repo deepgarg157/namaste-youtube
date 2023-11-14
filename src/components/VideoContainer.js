ed5f0255e6a04a99ed283e6e7433d45e5c8e19dd
unknown <deepgarg157@gmail.com>
2023-11-14 17:55:32 +0530

    namaste youtube

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,24 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { YOUTUBE_VIDEOS_API } from "../utility/constants";
 import VideoCard from "./VideoCard";
 
-const VideoContainer = () =>{
+const VideoContainer = () => {
 
-    useEffect(() => {
-        getVideos();
-      }, []);
-    
-      const getVideos = async () => {
-        const data = await fetch(YOUTUBE_VIDEOS_API);
-        const json = await data.json();
-        console.log(json);
-      };
+  const [videos, setVideos] = useState([])
 
-    return(
-        <div>
-            <VideoCard />
-        </div>
-    )
+  console.log(videos)
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  const getVideos = async () => {
+    const data = await fetch(YOUTUBE_VIDEOS_API);
+    const json = await data.json();
+    console.log(json);
+    setVideos(json.items)
+  };
+
+  return (
+    <div>
+      {videos.map((video) => <VideoCard key={video.id} cardInfo={video} />)}
+    </div>
+  )
 }
 
 export default VideoContainer;
--- src/utility/constants.js
+++ src/utility/constants.js
@@ -1,11 +1,11 @@
 export const user_Logo = "https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA0L3BmLWljb240LWppcjIwNjItcG9yLWwtam9iNzg4LnBuZw.png"
 
-const GOOGLE_API_KEY = "AIzaSyCJkn13f1ccRe3XtxQwMTjTPZ9K1PANj-c";
+const GOOGLE_API_KEY = "AIzaSyCaiyCj12p9flQwdHfTFkskLhY1dW6My54";
 
 export const LIVE_CHAT_COUNT = 25;
 
 export const YOUTUBE_VIDEOS_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&key=" + GOOGLE_API_KEY;
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=" + GOOGLE_API_KEY;
 
 
 export const YOUTUBE_SEARCH_API =
